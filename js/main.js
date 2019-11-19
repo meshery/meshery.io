e5d6acb0a91d8d7dc6f1bd0b249c31e6906c5ce8
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-11-19 15:44:37 -0800

    - sticky header - initial community section - attempted to fix the subscribe form style

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function () {
+	var header = document.getElementById("mainHeader");
+
+	function changeHeader() {
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
+	}
+
+	var didScroll = false;
+
+	window.addEventListener('scroll', function() {
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+			didScroll = false;
+			changeHeader();
+		}
+	}, 100);
+
+    changeHeader();
+    
+    document.getElementById("open-nav").onclick = function () {
+        document.body.classList.toggle("nav-open");
+        return false;
+    };
+})();
