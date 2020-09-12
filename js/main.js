30885d111891d81395e2ecb5f53140de5302aa9c
kolade Afeez <daisedalok@gmail.com>
2020-09-12 23:04:13 +0100

    program page background modified
    program cards behaviour changed
    
    Signed-off-by: kolade Afeez <daisedalok@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+
 (function () {
 	var header = document.getElementById("mainHeader");
-
 	function changeHeader() {
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		var scrollBuffer = 50;
-		if (window.location.pathname !== "/"){
+		if (window.location.pathname !== "/" && window.location.pathname == "/programs") {
+			console.log(window.location.pathname !== "/" || window.location.pathname !== "/programs")
 			scrollBuffer = 0;
 		}
 		header.classList.toggle("header-background", scrollTop >= scrollBuffer);
@@ -12,16 +13,16 @@
 
 	var didScroll = false;
 
-	window.addEventListener('scroll', function() {
+	window.addEventListener('scroll', function () {
 		didScroll = true;
 	});
 
-	setInterval(function() {
+	setInterval(function () {
 		if (didScroll) {
 			didScroll = false;
 			changeHeader();
 		}
 	}, 100);
 
-    changeHeader();
+	changeHeader();
 })();
