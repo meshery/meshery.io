d14c28b4b0093feb9aab4dff095ca24abef973e6
kolade Afeez <daisedalok@gmail.com>
2020-09-13 15:37:25 +0100

    fix gsod 2020 page break
    Signed-off-by: kolade Afeez <daisedalok@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-
 (function () {
-	var header = document.getElementById("mainHeader");
-	function changeHeader() {
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		var scrollBuffer = 50;
-		if (window.location.pathname !== "/" && window.location.pathname == "/programs") {
-			console.log(window.location.pathname !== "/" || window.location.pathname !== "/programs")
-			scrollBuffer = 0;
-		}
-		header.classList.toggle("header-background", scrollTop >= scrollBuffer);
-	}
+  var header = document.getElementById("mainHeader");
+  function changeHeader() {
+    var scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollBuffer = 50;
 
-	var didScroll = false;
+    if (
+      window.location.pathname == "/" &&
+      window.location.pathname.includes("/programs")
+    ) {
+      scrollBuffer = 0;
+    }
+    header.classList.toggle("header-background", scrollTop >= scrollBuffer);
+  }
 
-	window.addEventListener('scroll', function () {
-		didScroll = true;
-	});
+  var didScroll = false;
 
-	setInterval(function () {
-		if (didScroll) {
-			didScroll = false;
-			changeHeader();
-		}
-	}, 100);
+  window.addEventListener("scroll", function () {
+    didScroll = true;
+  });
 
-	changeHeader();
+  setInterval(function () {
+    if (didScroll) {
+      didScroll = false;
+      changeHeader();
+    }
+  }, 100);
+
+  changeHeader();
 })();
