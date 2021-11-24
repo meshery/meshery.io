b057b58024cb5cb10350c200b0d6f8fa197da411
Aditya Chaterjee <speak2adi@gmail.com>
2021-11-24 20:42:40 +0530

    added an initial commit for the catalog
    Signed-off-by: Aditya Chaterjee <speak2adi@gmail.com>

--- assets/js/talks-presentations.js
+++ assets/js/talks-presentations.js
@@ -1,25 +1,43 @@
-document.addEventListener('DOMContentLoaded', function(event){
-    var modal = document.querySelector('.modal-window');
-    var btnOpen = document.querySelector('.link');
-    var btnClose = document.querySelector('.close');
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.visiblity = "hidden";
-            modal.style.opacity = "0";
-            modal.style.pointerEvents= "none";
-            console.log('clicked');
-        }
-        if(event.target == btnClose){
-            modal.style.visiblity = "hidden";
-            modal.style.opacity = "0";
-            modal.style.pointerEvents= "none";
-            console.log('click');
-        }
-        if(event.target == btnOpen){
-            modal.style.visibility = "visible";
-            modal.style.opacity = "1";
-            modal.style.pointerEvents="auto";
-            console.log('click');
+// document.addEventListener('DOMContentLoaded', function(event){
+    var modal = document.querySelectorAll('.modal-window');
+    var btnOpen = document.querySelectorAll('.link');
+    var btnClose = document.querySelectorAll('.close');
+    var iframe = document.querySelectorAll('iframe')
+    var cont = document.querySelectorAll('.talks')
+
+
+    console.log(cont);
+    for(i=0;i<cont.length;i++){
+        console.log(cont[i]);
+        if(i%2!=0){
+            cont[i].style.flexDirection = "row-reverse";
         }
     }
-});
+    console.log(modal);
+    for(let i in modal){
+        console.log(modal[i]);
+    modal[i].addEventListener('click',function(){
+        modal[i].style.visiblity = "hidden";
+        modal[i].style.opacity = "0";
+        modal[i].style.pointerEvents= "none";
+        src = iframe[i].getAttribute('src');
+        iframe[i].setAttribute('src', '');
+        iframe[i].setAttribute('src',src);
+        console.log('clicked outside');
+
+    });
+    btnClose[i].addEventListener('click',function(){
+        modal[i].style.visiblity = "hidden";
+        modal[i].style.opacity = "0";
+        modal[i].style.pointerEvents= "none";
+        console.log('clicked btn');
+    });
+    btnOpen[i].addEventListener('click',function(){
+        modal[i].style.visibility = "visible";
+        modal[i].style.opacity = "1";
+        modal[i].style.pointerEvents="auto";
+        console.log('click btn open');
+        console.log(modal);
+    });
+    }
+// });
