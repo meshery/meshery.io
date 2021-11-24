0889430ed1e2380c3848740e2d9d4aa52d8f12a5
Reedham Pujara <reedhamjpujara2092001@gmail.com>
2021-11-25 00:58:14 +0530

    Reseting the source to stop video from playing when modal is closed
    Signed-off-by: Reedham Pujara <reedhamjpujara2092001@gmail.com>

--- assets/js/talks-presentations.js
+++ assets/js/talks-presentations.js
@@ -2,24 +2,28 @@ document.addEventListener('DOMContentLoaded', function(event){
     var modal = document.querySelector('.modal-window');
     var btnOpen = document.querySelector('.link');
     var btnClose = document.querySelector('.close');
+    var iframe = document.getElementById('#iframe');
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.visiblity = "hidden";
             modal.style.opacity = "0";
             modal.style.pointerEvents= "none";
-            console.log('clicked');
+            // Resets the src so that the video stops playing
+            var iframeSrc = iframe.src;
+		    iframe.src = iframeSrc;
         }
         if(event.target == btnClose){
             modal.style.visiblity = "hidden";
             modal.style.opacity = "0";
             modal.style.pointerEvents= "none";
-            console.log('click');
+            // Resets the src so that the video stops playing
+            var iframeSrc = iframe.src;
+		    iframe.src = iframeSrc;
         }
         if(event.target == btnOpen){
             modal.style.visibility = "visible";
             modal.style.opacity = "1";
             modal.style.pointerEvents="auto";
-            console.log('click');
         }
     }
 });
