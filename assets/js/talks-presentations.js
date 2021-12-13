54cf87a930cef4a35d3b1bff099e0631ebdea8cf
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-12-13 14:06:56 +0530

    update : talks-and-presentation page
    Signed-off-by: shubham-tyagi <raj.tyagi2000@gmail.com>

--- assets/js/talks-presentations.js
+++ assets/js/talks-presentations.js
@@ -1,25 +1,51 @@
 document.addEventListener('DOMContentLoaded', function(event){
-    var modal = document.querySelector('.modal-window');
+    var modal = document.querySelectorAll('.modal-window');
     var btnOpen = document.querySelector('.link');
-    var btnClose = document.querySelector('.close');
+    var btnClose = document.querySelectorAll('.close');
+    var recordingLink = document.querySelectorAll('.recording-link')
+    // recordingLink = recordingLink.unshift(btnOpen);
+    console.log(recordingLink);
     window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.visiblity = "hidden";
-            modal.style.opacity = "0";
-            modal.style.pointerEvents= "none";
+        for(let i=0;i<modal.length;i++){
+            if (event.target == modal[i+1]) {
+                modal[i+1].style.visiblity = "hidden";
+                modal[i+1].style.opacity = "0";
+                modal[i+1].style.pointerEvents= "none";
+                console.log('clicked');
+            }
+            if(event.target == btnClose[i+1]){
+                modal[i+1].style.visiblity = "hidden";
+                modal[i+1].style.opacity = "0";
+                modal[i+1].style.pointerEvents= "none";
+                console.log('click');
+            }
+            
+            if(event.target == recordingLink[i]){
+                modal[i+1].style.visibility = "visible";
+                modal[i+1].style.opacity = "1";
+                modal[i+1].style.pointerEvents="auto";
+                console.log('click');
+            }
+        }
+
+        if(event.target == btnOpen){
+            modal[0].style.visibility = "visible";
+            modal[0].style.opacity = "1";
+            modal[0].style.pointerEvents="auto";
+            console.log('click');
+        }
+        if (event.target == modal[0]) {
+            modal[0].style.visiblity = "hidden";
+            modal[0].style.opacity = "0";
+            modal[0].style.pointerEvents= "none";
             console.log('clicked');
         }
-        if(event.target == btnClose){
-            modal.style.visiblity = "hidden";
-            modal.style.opacity = "0";
-            modal.style.pointerEvents= "none";
+        if(event.target == btnClose[0]){
+            modal[0].style.visiblity = "hidden";
+            modal[0].style.opacity = "0";
+            modal[0].style.pointerEvents= "none";
             console.log('click');
         }
-        if(event.target == btnOpen){
-            modal.style.visibility = "visible";
-            modal.style.opacity = "1";
-            modal.style.pointerEvents="auto";
-            console.log('click');
-        }
+        
     }
 });
