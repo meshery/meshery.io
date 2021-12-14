dbce2221561e3e562304e0176cd222d5855d2ef3
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-12-14 17:00:09 +0530

    update talks and presentation page : Did the required changes
    Signed-off-by: shubham-tyagi <raj.tyagi2000@gmail.com>

--- assets/js/talks-presentations.js
+++ assets/js/talks-presentations.js
@@ -1,51 +1,59 @@
-document.addEventListener('DOMContentLoaded', function(event){
-    var modal = document.querySelectorAll('.modal-window');
-    var btnOpen = document.querySelector('.link');
-    var btnClose = document.querySelectorAll('.close');
-    var recordingLink = document.querySelectorAll('.recording-link')
-    // recordingLink = recordingLink.unshift(btnOpen);
-    console.log(recordingLink);
-    window.onclick = function(event) {
-        for(let i=0;i<modal.length;i++){
-            if (event.target == modal[i+1]) {
-                modal[i+1].style.visiblity = "hidden";
-                modal[i+1].style.opacity = "0";
-                modal[i+1].style.pointerEvents= "none";
-                console.log('clicked');
-            }
-            if(event.target == btnClose[i+1]){
-                modal[i+1].style.visiblity = "hidden";
-                modal[i+1].style.opacity = "0";
-                modal[i+1].style.pointerEvents= "none";
-                console.log('click');
-            }
-            
-            if(event.target == recordingLink[i]){
-                modal[i+1].style.visibility = "visible";
-                modal[i+1].style.opacity = "1";
-                modal[i+1].style.pointerEvents="auto";
-                console.log('click');
-            }
-        }
+var modal = document.querySelectorAll('.modal-window');
+var btnOpen = document.querySelector('.link');
+var btnClose = document.querySelectorAll('.close');
+var recordingLink = document.querySelectorAll('.recording-link')
+var n = recordingLink.length;
 
-        if(event.target == btnOpen){
-            modal[0].style.visibility = "visible";
-            modal[0].style.opacity = "1";
-            modal[0].style.pointerEvents="auto";
-            console.log('click');
-        }
-        if (event.target == modal[0]) {
-            modal[0].style.visiblity = "hidden";
-            modal[0].style.opacity = "0";
-            modal[0].style.pointerEvents= "none";
-            console.log('clicked');
-        }
-        if(event.target == btnClose[0]){
-            modal[0].style.visiblity = "hidden";
-            modal[0].style.opacity = "0";
-            modal[0].style.pointerEvents= "none";
-            console.log('click');
-        }
-        
-    }
+for(let i=0;i<n;i++){
+    console.log(modal[i]);
+
+    modal[i].addEventListener('click',function(){
+        modal[i].style.visiblity = "hidden";
+        modal[i].style.opacity = "0";
+        modal[i].style.pointerEvents= "none";
+        console.log('clicked outside');
+
+    });
+
+    btnClose[i].addEventListener('click',function(){
+        modal[i].style.visiblity = "hidden";
+        modal[i].style.opacity = "0";
+        modal[i].style.pointerEvents= "none";
+        console.log('clicked btn');
+    });
+
+    recordingLink[i].addEventListener('click',function(){
+        modal[i+1].style.visibility = "visible";
+        modal[i+1].style.opacity = "1";
+        modal[i+1].style.pointerEvents="auto";
+        console.log('click btn open');
+        console.log(modal);
+    });
+}
+
+
+// no of recordingLink = n and no of modals = n+1; 
+// n+1 because of the one on top apart from the table
+// nth modal (0 indexed) is not traversed in the loop because if we do that we would also have to traverse nth recording link which is not present and an error would appear on console.
+
+modal[n].addEventListener('click',function(){
+    modal[n].style.visiblity = "hidden";
+    modal[n].style.opacity = "0";
+    modal[n].style.pointerEvents= "none";
+    console.log('clicked outside');
+
+});
+btnClose[n].addEventListener('click',function(){
+    modal[n].style.visiblity = "hidden";
+    modal[n].style.opacity = "0";
+    modal[n].style.pointerEvents= "none";
+    console.log('clicked btn');
+});
+
+btnOpen.addEventListener('click',function(){
+    modal[0].style.visibility = "visible";
+    modal[0].style.opacity = "1";
+    modal[0].style.pointerEvents="auto";
+    console.log('click btn open');
+    console.log(modal);
 });
