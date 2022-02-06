d0afbb4667c24129b572bf4d0cccd750c4ccff93
Susmita Dey <susmitadey475@gmail.com>
2022-02-06 10:50:46 +0530

    Solved video modal issue

--- assets/js/video-modal.js
+++ assets/js/video-modal.js
@@ -5,28 +5,34 @@ var modal = document.getElementById("modal");
 var btn = document.getElementById("video-modal");
 
 // Get the video control
-var video = document.getElementById("video");
+var vid = document.getElementById("video");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-video")[0];
+// Get the <h3> element that closes the modal
+var cross = document.getElementsByClassName("close-video")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// When the user clicks the button, open the modal
+btn.onclick = function () {
   modal.style.display = "block";
-  video.play();
-}
+  vid.play();
+};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  video.pause();
-  modal.style.display = "none";
-
-}
+// When the user clicks on <h3> (x), close the modal
+cross.addEventListener(
+  "click",
+  function () {
+    vid.pause();
+    modal.style.display = "none";
+    while (cross.firstChild) {
+      cross.removeChild(cross.firstChild);
+    }
+  },
+  false
+);
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
-    video.pause();
+    vid.pause();
     modal.style.display = "none";
   }
-}
+};
