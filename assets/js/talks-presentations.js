662ca5bbf97b3ef7bfc5eb6d06bc76d9d6831550
Samyak Shah <samyakshah3008@gmail.com>
2023-06-18 14:18:49 +0530

    Refactor: add ES6 functions
    Signed-off-by: Samyak Shah <samyakshah3008@gmail.com>

--- assets/js/talks-presentations.js
+++ assets/js/talks-presentations.js
@@ -5,14 +5,14 @@ var btnClose = document.querySelectorAll('.close');
 var recordingLink = document.querySelectorAll('.recording-link')
 var n = recordingLink.length;
 
-window.addEventListener("click", function(event){
+window.addEventListener("click", (event)=> {
     if(event.target == talkPresentationModal){
         talkPresentationModal.style.visibility = "hidden";
        }
 })
 
 for(let i=0;i<n;i++){
-    btnClose[i].addEventListener('click',function(){
+    btnClose[i].addEventListener('click', ()=> {
         modal[i].style.visiblity = "hidden";
         modal[i].style.opacity = "0";
         modal[i].style.pointerEvents= "none";
@@ -21,19 +21,18 @@ for(let i=0;i<n;i++){
         iframeTEST.src = src;
     });
 
-    recordingLink[i].addEventListener('click',function(){
+    recordingLink[i].addEventListener('click', ()=> {
         modal[i+1].style.visibility = "visible";
         modal[i+1].style.opacity = "1";
         modal[i+1].style.pointerEvents="auto";
     });
 }
 
-
 // no of recordingLink = n and no of modals = n+1; 
 // n+1 because of the one on top apart from the table
 // nth modal (0 indexed) is not traversed in the loop because if we do that we would also have to traverse nth recording link which is not present and an error would appear on console.
 
-btnClose[n].addEventListener('click',function(){
+btnClose[n].addEventListener('click', ()=> {
     modal[n].style.visiblity = "hidden";
     modal[n].style.opacity = "0";
     modal[n].style.pointerEvents= "none";
@@ -42,10 +41,8 @@ btnClose[n].addEventListener('click',function(){
     iframeTEST.src = src;
 });
 
-btnOpen.addEventListener('click',function(){
+btnOpen.addEventListener('click', ()=> {
     modal[0].style.visibility = "visible";
     modal[0].style.opacity = "1";
     modal[0].style.pointerEvents="auto";
 });
-
-
