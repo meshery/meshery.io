3ba5f9ac0613b546249d22b20d999df4fe4dbfc4
dusdjhyeon <dusdj0813@gmail.com>
2024-06-20 16:04:27 +0900

    feat: process error by meshkit error
    Signed-off-by: dusdjhyeon <dusdj0813@gmail.com>

--- assets/actions/package.go
+++ assets/actions/package.go
@@ -3,32 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
-	"log"
 	"io/ioutil"
-	"strings"
+	"log"
 	"net/http"
-	"time"
+	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"gopkg.in/yaml.v3"
 
-	"github.com/layer5io/meshkit/utils/catalog"
 	"github.com/layer5io/meshkit/models/catalog/v1alpha1"
+	"github.com/layer5io/meshkit/utils/catalog"
 )
 
 // CatalogPattern represents the catalog pattern structure
 type CatalogPattern struct {
-	ID           string `json:"id"`
-	Name         string `json:"name"`
-	PatternFile  string `json:"pattern_file"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	PatternFile string `json:"pattern_file"`
 	CatalogData struct {
-        PatternInfo   string        `json:"pattern_info"`
-        PatternCaveats string      `json:"pattern_caveats"`
-        Type          string        `json:"type"`
-        ImageURL      interface{}   `json:"imageURL"`
-        Compatibility []string      `json:"compatibility"`
-    } `json:"catalog_data"`
+		PatternInfo    string      `json:"pattern_info"`
+		PatternCaveats string      `json:"pattern_caveats"`
+		Type           string      `json:"type"`
+		ImageURL       interface{} `json:"imageURL"`
+		Compatibility  []string    `json:"compatibility"`
+	} `json:"catalog_data"`
 	UserID string `json:"user_id"`
 }
 
@@ -45,17 +45,39 @@ const (
 	mesheryCatalogFilesDir = "catalog"
 )
 
+var (
+	ErrUnmarshalCatalogPatternCode = "test_code"
+	ErrProcessPatternCode          = "test_code"
+)
+
 func main() {
 	token := os.Getenv("GH_ACCESS_TOKEN")
+	log, err := logger.New("mesheryio_package", logger.Options{
+		Format:   logger.SyslogLogFormat,
+		LogLevel: 5,
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	catalogPatterns := fetchCatalogPatterns()
 
 	var patterns []CatalogPattern
 	if err := json.Unmarshal(catalogPatterns, &patterns); err != nil {
-		log.Fatalf("Error parsing catalog patterns: %v", err)
+		log.Error(meshkitErrors.New(ErrUnmarshalCatalogPatternCode, meshkitErrors.Alert, []string{"Unable to unmarshal the catalog pattern"}, []string{err.Error()}, []string{}, []string{}))
+		return
 	}
 
 	for _, pattern := range patterns {
-		processPattern(pattern, token)
+		if err := processPattern(pattern, token); err != nil {
+			log.Error(meshkitErrors.New(ErrProcessPatternCode, meshkitErrors.Alert,
+				[]string{"unable to process catalog pattern"},
+				[]string{err.Error()},
+				[]string{"fail to read/write file", "error regarding user info"},
+				[]string{"check the catalog pattern file", "check for updated files"},
+			))
+		}
 		fmt.Println(pattern.ID)
 	}
 }
@@ -63,41 +85,42 @@ func main() {
 func fetchCatalogPatterns() []byte {
 	resp, err := http.Get(fmt.Sprintf("%s/api/catalog/content/pattern", mesheryCloudBaseURL))
 	if err != nil {
-		log.Fatalf("Error connecting to Meshery Cloud: %v\n", err)
+		log.Printf("Error connecting to Meshery Cloud: %v\n", err)
 		return nil
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Error reading response body: %v\n", err)
+		log.Printf("Error reading response body: %v\n", err)
 		return nil
 	}
 	return body
 }
 
-func processPattern(pattern CatalogPattern, ghAccessToken string) {
+func processPattern(pattern CatalogPattern, ghAccessToken string) error {
 	patternImageURL := getPatternImageURL(pattern)
 	patternType := getPatternType(string(pattern.CatalogData.Type))
 	patternInfo := getStringOrEmpty(pattern.CatalogData.PatternInfo)
 	patternCaveats := getStringOrEmpty(pattern.CatalogData.PatternCaveats)
 
 	compatibilityStrings := make([]string, len(pattern.CatalogData.Compatibility))
-    for i, v := range pattern.CatalogData.Compatibility {
-        compatibilityStrings[i] = string(v) 
-    }
+	for i, v := range pattern.CatalogData.Compatibility {
+		compatibilityStrings[i] = string(v)
+	}
 	compatibility := getCompatibility(compatibilityStrings)
 
 	dir := filepath.Join("..", "..", "collections", "_catalog", patternType)
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		fmt.Printf("%s doesn't exist. Creating directory...\n", patternType)
 		os.MkdirAll(dir, 0755)
 	}
 
 	if err := writePatternFile(pattern, patternType, patternInfo, patternCaveats, compatibility, patternImageURL); err != nil {
-		log.Printf("Fialed to write pattern file")
+		return err
 	}
 	invokeGitHubAction(pattern.ID, patternImageURL, ghAccessToken)
+
+	return nil
 }
 
 func getPatternImageURL(pattern CatalogPattern) string {
@@ -157,29 +180,6 @@ func writePatternFile(pattern CatalogPattern, patternType, patternInfo, patternC
 	format := "2006-01-02 15:04:05Z"
 	currentDateTime, err := time.Parse(format, time.Now().UTC().Format(format))
 
-	var snapshotURL []string
-    switch v := pattern.CatalogData.ImageURL.(type) {
-    case string:
-        snapshotURL = []string{v}
-    case []string:
-        snapshotURL = v
-    default:
-        snapshotURL = []string{}
-    }
-
-	convertedCompatibility := make([]v1alpha1.CatalogDataCompatibility, len(pattern.CatalogData.Compatibility))
-    for i, compat := range pattern.CatalogData.Compatibility {
-        convertedCompatibility[i] = v1alpha1.CatalogDataCompatibility(compat)
-    }
-
-    convertedCatalogData := &v1alpha1.CatalogData{
-        PatternInfo:    pattern.CatalogData.PatternInfo,
-        PatternCaveats: pattern.CatalogData.PatternCaveats,
-        Type:           v1alpha1.CatalogDataType(pattern.CatalogData.Type),
-        SnapshotURL:    snapshotURL,
-        Compatibility:  convertedCompatibility,
-    }
-
 	artifactHubPkg := catalog.BuildArtifactHubPkg(pattern.Name, filepath.Join(dir, "deploy.yml"), pattern.UserID, "1.0.0", currentDateTime.Format(time.RFC3339), &pattern.CatalogData)
 	data, err := yaml.Marshal(artifactHubPkg)
 	if err != nil {
@@ -212,7 +212,7 @@ downloadLink: %s/deploy.yml
 ---`, pattern.Name, pattern.UserID, userFullName, userInfo.AvatarURL, patternType, compatibility, pattern.ID, patternImageURL, patternInfo, patternCaveats, mesheryCatalogFilesDir, pattern.ID, pattern.ID)
 
 	ioutil.WriteFile(fmt.Sprintf(filepath.Join("..", "..", "collections", "_catalog", patternType, pattern.ID+".md")), []byte(content), 0644)
-	
+
 	return nil
 }
 
@@ -243,7 +243,7 @@ func invokeGitHubActions(contentID, assetLocation string, ghAccessToken string) 
 		return
 	}
 	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", "Bearer "+ ghAccessToken)
+	req.Header.Set("Authorization", "Bearer "+ghAccessToken)
 	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
 
 	client := &http.Client{}
