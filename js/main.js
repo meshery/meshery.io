160ec38b10c311561c87c55841b15d6302f117df
Sourya07 <singhsourya137@gmail.com>
2026-01-23 19:12:28 +0530

    fix: taken the repo struture only for eslint
    Signed-off-by: Sourya07 <singhsourya137@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 (function () {
-  var header = document.getElementById('mainHeader');
-  function changeHeader() {
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var scrollBuffer = 50;
+var header = document.getElementById('mainHeader');
+function changeHeader() {
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  var scrollBuffer = 50;
 
-    if (
-      window.location.pathname === '/' ||
+  if (
+    window.location.pathname === '/' ||
       window.location.pathname.includes('/programs')
-    ) {
-      scrollBuffer = 0;
-    }
-    header.classList.toggle('header-shadow', scrollTop >= scrollBuffer);
+  ) {
+    scrollBuffer = 0;
   }
+  header.classList.toggle('header-shadow', scrollTop >= scrollBuffer);
+}
 
-  var didScroll = false;
+var didScroll = false;
 
-  window.addEventListener('scroll', function () {
-    didScroll = true;
-  });
+window.addEventListener('scroll', function () {
+  didScroll = true;
+});
 
-  setInterval(function () {
-    if (didScroll) {
-      didScroll = false;
-      changeHeader();
-    }
-  }, 100);
+setInterval(function () {
+  if (didScroll) {
+    didScroll = false;
+    changeHeader();
+  }
+}, 100);
 
-  changeHeader();
+changeHeader();
 })();
 
 var btnscroll = $('#button-scroll-to-up');
