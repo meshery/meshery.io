06deccac736814deb3ea182faac6381092b385d2
Sourya07 <singhsourya137@gmail.com>
2026-01-27 15:17:57 +0530

    chore: this commit only about eslint package
    Signed-off-by: Sourya07 <singhsourya137@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
 (function () {
-var header = document.getElementById('mainHeader');
-function changeHeader() {
-  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  var scrollBuffer = 50;
+  var header = document.getElementById("mainHeader");
+  function changeHeader() {
+    var scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollBuffer = 50;
 
-  if (
-    window.location.pathname === '/' ||
-      window.location.pathname.includes('/programs')
-  ) {
-    scrollBuffer = 0;
+    if (
+      window.location.pathname === "/" ||
+      window.location.pathname.includes("/programs")
+    ) {
+      scrollBuffer = 0;
+    }
+    header.classList.toggle("header-shadow", scrollTop >= scrollBuffer);
   }
-  header.classList.toggle('header-shadow', scrollTop >= scrollBuffer);
-}
 
-var didScroll = false;
+  var didScroll = false;
 
-window.addEventListener('scroll', function () {
-  didScroll = true;
-});
+  window.addEventListener("scroll", function () {
+    didScroll = true;
+  });
 
-setInterval(function () {
-  if (didScroll) {
-    didScroll = false;
-    changeHeader();
-  }
-}, 100);
+  setInterval(function () {
+    if (didScroll) {
+      didScroll = false;
+      changeHeader();
+    }
+  }, 100);
 
-changeHeader();
+  changeHeader();
 })();
 
 var btnscroll = $('#button-scroll-to-up');
 
-$(window).scroll(function () {
+$(window).scroll(function() {
   if ($(window).scrollTop() > 0) {
     btnscroll.addClass('show');
   } else {
@@ -39,13 +40,16 @@ $(window).scroll(function () {
   }
 });
 
-btnscroll.on('click', function (e) {
+btnscroll.on('click', function(e) {
   e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '0');
+  $('html, body').animate({scrollTop:0}, '0');
 });
 
-$('#slider').on('input change', (e) => {
+
+
+$("#slider").on("input change", (e)=>{
   const sliderPos = e.target.value;
-  $('.foreground-img').css('width', `${sliderPos}%`);
-  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`);
+  $('.foreground-img').css('width', `${sliderPos}%`)
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
 });
+
--- js/pagination.js
+++ js/pagination.js
@@ -1,11 +1,13 @@
-var url = window.location.href;
+url = window.location.href
 
-var page = url.slice(-1);
+page = url.slice(-1)
 
-var page1div = document.getElementById('page1');
-var page2div = document.getElementById('page2');
+page1div = document.getElementById("page1")
+page2div = document.getElementById("page2")
 
-if (page == 2) {
-  page1div.style.display = 'none';
-  page2div.style.display = 'block';
+
+
+if(page==2){
+    page1div.style.display = "none"
+    page2div.style.display = "block"
 }
