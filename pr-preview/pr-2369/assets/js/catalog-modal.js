ee5e38ae68a6bd7f14494b10d4828f731a8b1338
hortison <hortison@users.noreply.github.com>
2026-05-26 20:10:03 +0000

    Deploy preview for PR 2369 🛫

---
+++ pr-preview/pr-2369/assets/js/catalog-modal.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contentElements = document.querySelectorAll(".contentdata");
+  const readMoreElements = document.querySelectorAll(".read-more");
+
+  for (let i = 0; i < contentElements.length; i++) {
+    const content = contentElements[i];
+    const readMore = readMoreElements[i];
+
+    const numberOfLines = getNumberOfLines(content);
+
+    if (numberOfLines > 3) {
+      readMore.style.display = 'inline';
+    } else {
+      readMore.style.display = 'none';
+    }
+  }
+});
+
+function getNumberOfLines(element) {
+  const clone = element.cloneNode(true);
+  clone.style.visibility = 'hidden';
+  clone.style.position = 'absolute';
+  clone.style.overflow = 'visible';
+  document.body.appendChild(clone);
+
+  const lineHeight = parseFloat(window.getComputedStyle(clone).lineHeight);
+  const height = clone.clientHeight;
+
+  document.body.removeChild(clone);
+
+  const numberOfLines = Math.round(height / lineHeight);
+  return numberOfLines;
+}
