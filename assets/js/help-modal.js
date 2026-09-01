0ac2d101a2106c41e91d8d79210e6cf8fb3ca020
Janakiram <baddulabablu05@gmail.com>
2026-09-01 21:09:21 +0530

    fix: improve catalog modal accessibility
    Signed-off-by: Janakiram <baddulabablu05@gmail.com>

--- assets/js/help-modal.js
+++ assets/js/help-modal.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("myModal");
 const button = document.getElementById("myBtn");
 const closeButton = document.getElementsByClassName("closed")[0];
+let previouslyFocusedElement = null;
 
 function setCatalogInteraction(enabled) {
   const catalogContent = document.querySelector(".feature-container");
@@ -10,16 +11,35 @@ function setCatalogInteraction(enabled) {
 button.onclick = function () {
   modal.style.display = "flex";
   setCatalogInteraction(true);
+  previouslyFocusedElement = button;
+  closeButton.focus();
 };
 
 closeButton.onclick = function () {
   modal.style.display = "none";
   setCatalogInteraction(false);
+  if (previouslyFocusedElement) {
+    previouslyFocusedElement.focus();
+  }
+};
+
+modal.onkeydown = function (event) {
+  if (event.key === "Escape") {
+    event.preventDefault();
+    modal.style.display = "none";
+    setCatalogInteraction(false);
+    if (previouslyFocusedElement) {
+      previouslyFocusedElement.focus();
+    }
+  }
 };
 
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = "none";
     setCatalogInteraction(false);
+    if (previouslyFocusedElement) {
+      previouslyFocusedElement.focus();
+    }
   }
 };
